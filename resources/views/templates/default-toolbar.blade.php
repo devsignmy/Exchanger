dd4a3c123e9b2d99a871be405539655a3de044f3
Nizul Zaim <shimizukazunari93@gmail.com>
2015-05-07 00:37:34 +0800

    Cleaning Deafult Toolbar

--- resources/views/templates/default-toolbar.blade.php
+++ resources/views/templates/default-toolbar.blade.php
@@ -6,22 +6,7 @@
 		</a>
 		<div class="toolbar-title">Devsign.My</div>
 		<div style="padding-left: 16px; padding-right: 16px;">
-			<div class="pull-right">
-				<a class="icon-button ripple" href="javascript:" data-popup=".popup">
-					<i data-icon="bell" class="white"></i>
-				</a>
-				<div class="popup">
-					<div class="row">
-						<div class="col-xs-1">
-							<img src="/img/profile.png" class="img-responsive" />
-						</div>
-						<div class="col-xs">
 
-						</div>
-					</div>
-				</div>
-				<input type="checkbox" class="white-blue large" data-switch="" name="value_1" value="1" />
-			</div>
 		</div>
 
 	</div>
